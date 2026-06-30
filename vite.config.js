f8a0b62a350f1fdf732fdc6b3847b83335071b5c
Waleedch1011 <waleed.khalid551111@gmail.com>
2026-06-30 20:20:11 +0500

    Set PWA start_url to /quick

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
         background_color: '#0a0d14',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/quick',
+        scope: '/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
